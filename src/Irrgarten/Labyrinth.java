863a5874037c33fa7cb448664f3176ee28fa2f03
Moon <moonpyx6@gmail.com>
2023-10-20 13:08:50 +0200

    Terminada clase laberinto (en teoria bien)

--- src/Irrgarten/Labyrinth.java
+++ src/Irrgarten/Labyrinth.java
@@ -11,17 +11,23 @@ public class Labyrinth {
     private static int ROW = 0;
     private static int COL = 1;
     private int nRows, nCols, exitRow, exitCol;
+    private Monster[][] monsters;
+    private Player[][] players;
+    private char[][] labyrinth;
     
-    private char [][] labyrinth;
     public Labyrinth(int nRows, int nCols, int exitRow, int exitCol){
+     labyrinth = new char[nRows][nCols];
+     monsters = new Monster[nRows][nCols];
+     players = new Player[nRows][nCols];
         this.nRows = nRows;
         this.nCols = nCols;
         this.exitRow = exitRow;
         this.exitCol = exitCol;
-        this.labyrinth = new char[nRows][nCols];
         for (int i = 0; i<nRows; i++){
             for (int j = 0; j<nCols; j++){
                 labyrinth[i][j] = EMPTY_CHAR;
+                monsters[i][j] = null;
+                players[i][j] = null;
             }
         }
         labyrinth[exitRow][exitCol] = EXIT_CHAR;
@@ -32,13 +38,34 @@ public class Labyrinth {
         
     }
     public boolean haveAWinner(){
-        return false;
+        return players[exitRow][exitCol] != null;
     }
     public String toString(){
-        return null;
+        String result = "";
+         for (int i = 0; i<nRows; i++){
+            for (int j = 0; j<nCols; j++){
+                if (combatPos(i,j)){
+                    result += COMBAT_CHAR;
+                } else if(monsterPos(i,j)){
+                    result += MONSTER_CHAR;
+                } else{
+                     result +=labyrinth[i][j];
+                }
+                result += ' ';
+            }
+            result += "\n";
+        }
+         return result;
     }
     public void addMonster(int row, int col, Monster monster){
-  
+        if(canStepOn(row, col)){
+            monsters[row][col]= monster;
+            if (players[row][col]!= null){
+                labyrinth[row][col] = COMBAT_CHAR;
+            } else{
+                labyrinth[row][col] = MONSTER_CHAR;
+            }
+        }
     }
     public Monster putPlayer(Directions direction, Player player){
         return null;
@@ -51,28 +78,54 @@ public class Labyrinth {
         return null;
     }
     private boolean posOK (int row, int col){
-        return false;
+        return row >=0 && row < nRows && col >= 0 && col < nCols;
     }
     private boolean monsterPos(int row, int col){
-        return false;
+        return monsters[row][col]!=null;
     }
     private boolean exitPos(int row, int col){
-        return false;
+        return labyrinth[row][col]== EXIT_CHAR;
     }
      private boolean combatPos(int row, int col){
-        return false;
+        return monsters[row][col] != null && players[row][col] != null;
     }
      private boolean canStepOn(int row, int col){
-        return false;
+        return posOK(row, col) && (labyrinth[row][col] == EMPTY_CHAR || monsterPos(row, col) || exitPos(row, col));
     }
      private void updateOldPos(int row, int col){
-         
+         if (row >= 0 && row < nRows && col >= 0 && col < nCols) {
+                if (labyrinth[row][col]== COMBAT_CHAR){
+                    labyrinth[row][col]= MONSTER_CHAR;
+                } else{
+                    labyrinth[row][col] = EMPTY_CHAR;
+                }
+             
+         }
      }
      private int[] dir2Pos(int row, int col, Directions direction){
-         return null;
+         switch (direction) {
+        case UP:
+            row--;
+            break;
+        case DOWN:
+            row++;
+            break;
+        case LEFT:
+            col--;
+            break;
+        case RIGHT:
+            col++;
+            break;
+    }
+         return new int[]{row,col};
      }
      private int[] randomEmptyPos(){
-         return null;
+         int randomRow, randomCol;
+         do {
+             randomRow = Dice.randomPos(nRows);
+             randomCol = Dice.randomPos(nCols);
+         } while (labyrinth[randomRow][randomCol] != EMPTY_CHAR);   
+         return new int[]{randomRow, randomCol};
      }
      private Monster putPlayer2D(int oldRow, int OldCol, int row, int col, Player player){
          return null;
--- src/Irrgarten/TestP1.java
+++ src/Irrgarten/TestP1.java
@@ -1,18 +1,18 @@
-package Irrgarten;
-public class TestP1 {
-    public static void main(String[] args){
-        Weapon sword = new Weapon(5.0f, 3);
-        Shield shield = new Shield(4.0f, 2);
-        
-        Directions direction = Directions.LEFT;
-        GameCharacter character = GameCharacter.PLAYER;
-        int resurrectCount = 0;
-        for (int i = 0; i < 100; i++) {
-            if (Dice.resurrectPlayer()) {
-                resurrectCount++;
+    package Irrgarten;
+    public class TestP1 {
+        public static void main(String[] args){
+            Weapon sword = new Weapon(5.0f, 3);
+            Shield shield = new Shield(4.0f, 2);
+
+            Directions direction = Directions.LEFT;
+            GameCharacter character = GameCharacter.PLAYER;
+            int resurrectCount = 0;
+            for (int i = 0; i < 100; i++) {
+                if (Dice.resurrectPlayer()) {
+                    resurrectCount++;
+                }
             }
+            System.out.println("Número de resurrecciones: " + resurrectCount);
         }
-        System.out.println("Número de resurrecciones: " + resurrectCount);
+
     }
-    
-}
